a19a44700ea3eab0ac20a87ad9be7db9a7ff0e5a
ccshare <chen.cheng@myshare.io>
2018-05-27 14:08:46 +0800

    refine

--- src/file-up-down/vud.go
+++ src/file-up-down/vud.go
@@ -11,6 +11,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -43,7 +44,7 @@ func token(serverURL string, entryKey string, entryOp string) (string, error) {
 		log.Fatal(err)
 		return "", err
 	}
-	log.Printf("put token: %s", token.Token)
+
 	return token.Token, nil
 }
 
@@ -107,59 +108,65 @@ func md5sum(filename string) (string, error) {
 }
 
 func onlyUpload(serverURL, key, filename string) {
-	randKey := fmt.Sprintf("%s-sufix", key)
-	ptoken, err := token(serverURL, randKey, "put")
+	md5, err := md5sum(filename)
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
-	upload(serverURL, randKey, ptoken, filename)
-	umd5, err := md5sum(filename)
+	fileKey := fmt.Sprintf("%s-%s", key, md5)
+	ptoken, err := token(serverURL, fileKey, "put")
 	if err != nil {
-		log.Println("calc ufile md5 error: ", err)
+		log.Fatal(err)
+		return
 	}
-	fmt.Printf("file: %s, md5: %s", filename, umd5)
+	upload(serverURL, fileKey, ptoken, filename)
+
+	fmt.Printf("file: %s \nkey : %s \nmd5 : %s\n", filename, fileKey, md5)
 }
 
-func onlyDownload(serverURL, key, filename string) {
+func onlyDownload(serverURL, key, dir, filename string) {
 	gtoken, err := token(serverURL, key, "get")
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	var sfile string
+	var dfile string
 	if filename == "filename" {
-		sfile = fmt.Sprintf("file-%s.down", key)
+		dfile = filepath.Join(dir, fmt.Sprintf("file-%s.down", key))
 	} else {
-		sfile = filename
+		dfile = filename
 	}
-	download(serverURL, key, gtoken, sfile)
-	dmd5, err := md5sum(sfile)
+	download(serverURL, key, gtoken, dfile)
+	md5, err := md5sum(dfile)
 	if err != nil {
 		log.Println("calc sfile md5 error: ", err)
 	}
-	fmt.Printf("file: %s, md5: %s", filename, dmd5)
+
+	fmt.Printf("file: %s \nkey : %s \nmd5 : %s\n", dfile, key, md5)
 }
 
-func validateUploadDownload(serverURL string, key string, num uint) string {
-	rand.Seed(time.Now().Unix())
-	randValue := rand.Intn(8192)
+func validateUploadDownload(serverURL string, key string, dir string, num uint) {
 
-	ufile := fmt.Sprintf("ufile.%05d", randValue)
+	ufile := filepath.Join(dir, fmt.Sprintf("upload-file.%s", key))
+
 	file, err := os.OpenFile(ufile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		log.Fatal("OpenFile", err)
-		return "error"
+		return
 	}
 	defer file.Close()
 	var i uint
 	for i = 0; i < num; i++ {
-		randKey := fmt.Sprintf("%s-%04d-%05d", key, randValue, i)
-		if _, err := file.Write([]byte("Append some data")); err != nil {
-			log.Fatal("Write file")
+		randKey := fmt.Sprintf("%s-%08d", key, i)
+		content := fmt.Sprintf("Just test file contents with( %s )\n", randKey)
+		if _, err := file.Write([]byte(content)); err != nil {
+			log.Fatal("Write file", err)
+			break
 		}
 		ptoken, err := token(serverURL, randKey, "put")
 		if err != nil {
 			log.Fatal(err)
+			break
 		}
 		upload(serverURL, randKey, ptoken, ufile)
 		umd5, err := md5sum(ufile)
@@ -174,9 +181,9 @@ func validateUploadDownload(serverURL string, key string, num uint) string {
 			break
 		}
 
-		sfile := fmt.Sprintf("sfile.%d", i)
-		download(serverURL, randKey, gtoken, sfile)
-		dmd5, err := md5sum(sfile)
+		dfile := filepath.Join(dir, fmt.Sprintf("download-file.%d", i))
+		download(serverURL, randKey, gtoken, dfile)
+		dmd5, err := md5sum(dfile)
 		if err != nil {
 			log.Println("calc sfile md5 error: ", err)
 			break
@@ -185,35 +192,65 @@ func validateUploadDownload(serverURL string, key string, num uint) string {
 			log.Printf("checkmd5 failed  %s != %s", umd5, dmd5)
 		} else {
 			log.Printf("checkmd5 success %s == %s", umd5, dmd5)
-			os.Remove(sfile)
+			os.Remove(dfile)
 		}
 
 	}
-	return "finish"
+
 }
 
 func main() {
+	const VERSION = "version: 1.0.0"
 	host := flag.String("host", "localhost", "hostname")
 	port := flag.Int("port", 3000, "port")
 	key := flag.String("key", "key", "key")
 	ufile := flag.String("ufile", "filename", "upload filename")
-	sfile := flag.String("save", "filename", "save download filename")
-	num := flag.Uint("num", 1024, "upload/download times")
+	dfile := flag.String("dfile", "filename", "download filename")
+	num := flag.Uint("num", 1, "upload/download times")
 	debug := flag.Bool("debug", false, "enable/disable debug mode")
 	ignore := flag.Bool("i", false, "ignore failed validation")
+	version := flag.Bool("version", false, "show version")
 
 	flag.Parse()
+	if *version == true {
+		fmt.Printf("%s  %s\n", os.Args[0], VERSION)
+		return
+	}
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	logdir := filepath.Join(pwd, "log")
+	datadir := filepath.Join(pwd, "tmp")
+	_ = os.Mkdir(logdir, 0755)
+	_ = os.Mkdir(datadir, 0755)
+
+	rand.Seed(time.Now().Unix())
+	randValue := rand.Intn(8192)
+
+	logFilename := fmt.Sprintf("%s-%04d.log", os.Args[0], randValue)
+	logFilename = filepath.Join(logdir, logFilename)
+	logFile, logErr := os.OpenFile(logFilename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+	if logErr != nil {
+		fmt.Println("Fail to find", *logFile, "cServer start Failed")
+		os.Exit(1)
+	}
+	log.SetOutput(logFile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
 	serverURL := fmt.Sprintf("http://%s:%d", *host, *port)
-	fmt.Printf("key:%s, ufile:%s, dfile:%s debug: %v, num:%d, ignore:%v\n", *key, *ufile, *sfile, *debug, *num, *ignore)
-	log.Printf("key:%s, ufile:%s, dfile:%s debug: %v, num:%d, ignore:%v\n", *key, *ufile, *sfile, *debug, *num, *ignore)
+	fmt.Printf("server:%s, logfile:%s debug: %v, num:%d, ignore:%v\n", serverURL, logFilename, *debug, *num, *ignore)
+	log.Printf("server:%s, logfile:%s debug: %v, num:%d, ignore:%v\n", serverURL, logFilename, *debug, *num, *ignore)
 
 	if *ufile != "filename" {
 		onlyUpload(serverURL, *key, *ufile)
 	} else if *key != "key" {
-		onlyDownload(serverURL, *key, *sfile)
+		onlyDownload(serverURL, *key, datadir, *dfile)
 	} else {
-		validateUploadDownload(serverURL, *key, *num)
+		randKey := fmt.Sprintf("%s-%04d", *key, randValue)
+		validateUploadDownload(serverURL, randKey, datadir, *num)
 	}
 
 }
