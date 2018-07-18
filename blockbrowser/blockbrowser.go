c3edc346cc25f9f4d3a15baa5995d2540ba696c8
ccshare <chen.cheng@myshare.io>
2018-07-18 23:10:51 +0800

    Add

--- blockbrowser/blockbrowser.go
+++ blockbrowser/blockbrowser.go
@@ -28,14 +28,17 @@ const contractManager = "myshare-contract-manager"
 const lsName = "lsdata"
 const rsName = "rsdata"
 
-func printUsage(inspectCmd *flag.FlagSet, keyCmd *flag.FlagSet) {
+func printUsage(inspectCmd *flag.FlagSet, fileCmd *flag.FlagSet, dbCmd *flag.FlagSet) {
 	flag.Usage()
 	fmt.Println("  inspect")
 	fmt.Println("        Inspect infomation")
-	fmt.Println("  key")
-	fmt.Println("        Key convert")
+	fmt.Println("  file")
+	fmt.Println("        Read filesystem")
+	fmt.Println("  db")
+	fmt.Println("        Read DB")
 	inspectCmd.Usage()
-	keyCmd.Usage()
+	fileCmd.Usage()
+	dbCmd.Usage()
 }
 
 // Contract struct
@@ -69,29 +72,83 @@ func getCmIndexFromKey(key string, cm int) (int, error) {
 	return int(decoded[0]) % cm, nil
 }
 
-func inspect(root *string, key *string, sizeThreshold int, cmNum int) {
-	fileStoreRoot := filepath.Join(*root, fileStoreName)
-	cmRoot := filepath.Join(*root, contractManager)
-	lsRoot := filepath.Join(fileStoreRoot, lsName)
-	rsRoot := filepath.Join(fileStoreRoot, rsName)
+func inspectFile(root *string, key *string) {
 	inKey := internalKey(*key)
+	filename := filepath.Join(*root, inKey[0:3], inKey[3:6], inKey[6:])
+	fd, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fd.Close()
 
-	cmDb, err := leveldb.OpenFile(cmRoot, nil)
+	hash := sha256.New()
+	if _, err := io.Copy(hash, fd); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Block information:\n")
+	fmt.Printf("  store     : RS\n")
+	fmt.Printf("  inKey     : %s\n", inKey)
+	fmt.Printf("  path      : %s\n", filename)
+	fmt.Printf("  hash      : %x\n", hash.Sum(nil))
+}
+
+func inspectDb(root *string, key *string, cmNum int) {
+	inKey := internalKey(*key)
+	dbIndex, err := getCmIndexFromKey(inKey, cmNum)
+	if err != nil {
+		log.Println(err)
+	}
+	dbPath := filepath.Join(*root, strconv.Itoa(dbIndex))
+	lsDb, err := leveldb.OpenFile(dbPath, nil)
 	if err != nil {
 		fmt.Println("Open leveldb error: ", err)
+		return
+	}
+	defer lsDb.Close()
+
+	chunkIndex := 0
+	hash := sha256.New()
+	for {
+		chunkKey := getChunkKeyByIndex(inKey, chunkIndex)
+		ldata, err := lsDb.Get([]byte(chunkKey), nil)
+		if err != nil {
+			break
+		}
+		hash.Write(ldata)
+		chunkIndex++
+	}
+
+	if chunkIndex == 0 {
+		fmt.Printf("Block information(not find)\n")
+	} else {
+		fmt.Printf("Block information:\n")
+		fmt.Printf("  store     : LS\n")
+		fmt.Printf("  inKey     : %s\n", inKey)
+		fmt.Printf("  CM Index  : %d\n", dbIndex)
+		fmt.Printf("  Chunk NUM : %d\n", chunkIndex)
+		fmt.Printf("  hash      : %x\n", hash.Sum(nil))
+	}
+}
+
+func inspect(root *string, key *string, sizeThreshold int, cmNum int) {
+	cmRoot := filepath.Join(*root, contractManager)
+	cmDb, err := leveldb.OpenFile(cmRoot, nil)
+	if err != nil {
+		fmt.Println("Open contract leveldb error: ", err)
 		return
 	}
 	defer cmDb.Close()
 
 	cdata, err := cmDb.Get([]byte(*key), nil)
 	if err != nil {
-		fmt.Println("not find contract", *key, err)
+		fmt.Println("Not find contract of ", *key, err)
 		return
 	}
 
 	contract := Contract{}
 	if err := json.Unmarshal(cdata, &contract); err != nil {
-		fmt.Println("decode contract error: ", err)
+		fmt.Println("Decode contract error: ", err)
 		return
 	}
 
@@ -110,65 +167,13 @@ func inspect(root *string, key *string, sizeThreshold int, cmNum int) {
 			return
 		}
 	*/
-
+	fileStoreRoot := filepath.Join(*root, fileStoreName)
 	if contract.Size > sizeThreshold {
-		/**
-		 * key : 12345678985a0aa21c23f5abd2975a89b682abcd
-		 * path: 123/456/78985a0aa21c23f5abd2975a89b682abcd
-		 */
-		filename := filepath.Join(rsRoot, inKey[0:3], inKey[3:6], inKey[6:])
-		fd, err := os.Open(filename)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer fd.Close()
-
-		hash := sha256.New()
-		if _, err := io.Copy(hash, fd); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("Block information:\n")
-		fmt.Printf("  store     : RS\n")
-		fmt.Printf("  inKey     : %s\n", inKey)
-		fmt.Printf("  path      : %s\n", filename)
-		fmt.Printf("  hash      : %x\n", hash.Sum(nil))
+		rsRoot := filepath.Join(fileStoreRoot, rsName)
+		inspectFile(&rsRoot, key)
 	} else {
-		// getDB index get DB from key
-		dbIndex, err := getCmIndexFromKey(inKey, cmNum)
-		if err != nil {
-			log.Println(err)
-		}
-		dbPath := filepath.Join(lsRoot, strconv.Itoa(dbIndex))
-		lsDb, err := leveldb.OpenFile(dbPath, nil)
-		if err != nil {
-			fmt.Println("Open leveldb error: ", err)
-			return
-		}
-		defer lsDb.Close()
-
-		chunkIndex := 0
-		hash := sha256.New()
-		for {
-			chunkKey := getChunkKeyByIndex(inKey, chunkIndex)
-			ldata, err := lsDb.Get([]byte(chunkKey), nil)
-			if err != nil {
-				break
-			}
-			hash.Write(ldata)
-			chunkIndex++
-		}
-
-		if chunkIndex == 0 {
-			fmt.Printf("Block information(not find)\n")
-		} else {
-			fmt.Printf("Block information:\n")
-			fmt.Printf("  store     : LS\n")
-			fmt.Printf("  inKey     : %s\n", inKey)
-			fmt.Printf("  CM Index  : %d\n", dbIndex)
-			fmt.Printf("  Chunk NUM : %d\n", chunkIndex)
-			fmt.Printf("  hash      : %x\n", hash.Sum(nil))
-		}
+		lsRoot := filepath.Join(fileStoreRoot, lsName)
+		inspectDb(&lsRoot, key, cmNum)
 	}
 
 }
@@ -182,48 +187,70 @@ func main() {
 	inspectKey := inspectCmd.String("k", "", "Block key")
 	inspectRoot := inspectCmd.String("r", "", "Data root dir")
 	inspectSize := inspectCmd.Int("s", 102400, "Block size threshold")
-	inspectCmnum := inspectCmd.Int("n", 2, "CM number")
+	inspectDbNum := inspectCmd.Int("n", 2, "Chunk DB number")
 
-	keyCmd := flag.NewFlagSet("key", flag.ExitOnError)
-	keyKey := keyCmd.String("k", "", "block key")
+	fileCmd := flag.NewFlagSet("file", flag.ExitOnError)
+	fileKey := fileCmd.String("k", "", "block key")
+	fileRoot := inspectCmd.String("r", "", "Data root dir")
+
+	dbCmd := flag.NewFlagSet("db", flag.ExitOnError)
+	dbKey := dbCmd.String("k", "", "block key")
+	dbRoot := inspectCmd.String("r", "", "Data root dir")
+	dbNum := inspectCmd.Int("n", 2, "CM number")
 
 	if len(os.Args) < 2 {
-		printUsage(inspectCmd, keyCmd)
+		printUsage(inspectCmd, fileCmd, dbCmd)
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
 	case "inspect":
 		inspectCmd.Parse(os.Args[2:])
-	case "key":
-		keyCmd.Parse(os.Args[2:])
+	case "file":
+		fileCmd.Parse(os.Args[2:])
+	case "db":
+		dbCmd.Parse(os.Args[2:])
 	default:
 		flag.Parse()
 	}
 
 	if inspectCmd.Parsed() {
 		if *inspectKey == "" {
-			fmt.Println("Please supply the key using -key option.")
+			fmt.Println("Please supply the key with -k option.")
 			os.Exit(2)
 		} else if *inspectRoot == "" {
-			fmt.Println("Please supply the root using -root option.")
+			fmt.Println("Please supply the root dir with -r option.")
 			os.Exit(3)
 		}
-		inspect(inspectRoot, inspectKey, *inspectSize, *inspectCmnum)
-	} else if keyCmd.Parsed() {
-		if *keyKey == "" {
-			fmt.Println("Please supply the user using -user option.")
+		inspect(inspectRoot, inspectKey, *inspectSize, *inspectDbNum)
+	} else if fileCmd.Parsed() {
+		if *fileKey == "" {
+			fmt.Println("Please supply the key with -k option.")
 			os.Exit(2)
+		} else if *fileRoot == "" {
+			fmt.Println("Please supply the root dir with -r option.")
+			os.Exit(3)
 		}
-		fmt.Printf("internalKey: %s\n", internalKey(*keyKey))
+		fmt.Printf("internalKey: %s\n", internalKey(*fileKey))
+		inspectFile(fileRoot, fileKey)
+	} else if dbCmd.Parsed() {
+		if *dbKey == "" {
+			fmt.Println("Please supply the key with -k option.")
+			os.Exit(2)
+		} else if *dbRoot == "" {
+			fmt.Println("Please supply the root dir with -r option.")
+			os.Exit(3)
+		}
+		fmt.Printf("internalKey: %s\n", internalKey(*fileKey))
+		inspectDb(dbRoot, dbKey, *dbNum)
 	} else { // if flag.Parsed()
 		if true == *version {
 			fmt.Printf("%s  %s\n", filepath.Base(os.Args[0]), VERSION)
 		} else if true == *help {
-			printUsage(inspectCmd, keyCmd)
+			printUsage(inspectCmd, fileCmd, dbCmd)
 		} else {
 			fmt.Println("Unknow args ...")
-			printUsage(inspectCmd, keyCmd)
+			printUsage(inspectCmd, fileCmd, dbCmd)
 		}
 	}
 }
