4d092af5589ac2ee7ccc1f710083e35134793d4c
ccshare <chen.cheng@myshare.io>
2018-08-20 14:30:18 +0800

    Add args

--- server/tserver/tserver.go
+++ server/tserver/tserver.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"net/http"
 )
 
 
 func main() {
-	fmt.Println("hello")
+	host := flag.String("host", "0.0.0.0", "Listen address")
+	port := flag.Int("port", 80, "Listen port")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Println(*r)
+        })
+
+	flag.Parse()
+
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+	fmt.Printf("Listen on %s\n", addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
