ce4a85f763df4b288ee8819b7a16e3a313ff6f23
ccshare <chen.cheng@myshare.io>
2018-09-26 17:55:53 +0800

    refine

--- upnp/upnp.go
+++ upnp/upnp.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/NebulousLabs/go-upnp"
 	"log"
 )
 
 func main() {
+        fport := flag.Uint("port", 9902, "Listen port")
+        flag.Parse()
+	port := uint16(*fport)
+
 	// connect to router
 	d, err := upnp.Discover()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// forward a port
+	err = d.Forward(port, "myshare-upnp-checker")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("map port:%d success\n", port)
+
+	// un-forward a port
+	err = d.Clear(port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("unmap port:%d success\n", port)
 
 	// discover external IP
 	ip, err := d.ExternalIP()
@@ -19,20 +38,6 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Your external IP is:", ip)
-
-	// forward a port
-	err = d.Forward(9001, "upnp-checker")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("port map success")
-
-	// un-forward a port
-	err = d.Clear(9001)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("port unmap success")
 
 	// record router's location
 	loc := d.Location()
