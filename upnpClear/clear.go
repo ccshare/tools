5b52d5e854d8aeca2b2035c42ec4e726dd251bc8
ccshare <chen.cheng@myshare.io>
2018-10-19 12:39:41 +0800

    Add port flag

--- upnpClear/clear.go
+++ upnpClear/clear.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"flag"
 	"github.com/NebulousLabs/go-upnp"
 )
 
 func main() {
-	var maxPort uint16 = 30000
+	port := flag.Uint("port", 20000, "port")
+	max := flag.Uint("max", 30000, "max port")
+	flag.Parse()
+	if *max < *port || *port >math.MaxUint16 || *max > math.MaxUint16 {
+		fmt.Println("invalid port")
+		return
+	}
 	// connect to router
 	d, err := upnp.Discover()
 	if err != nil {
@@ -14,16 +22,17 @@ func main() {
 		return
 	}
 
-	for i := uint16(20000); i < maxPort; i++ {
+	for i := *port; i < *max; i++ {
+		iPort := uint16(i)
 		// un-forward a port
-		enabled, err := d.IsForwardedTCP(i)
+		enabled, err := d.IsForwardedTCP(iPort)
 		if err != nil {
 			continue
 		}
 		if enabled == true {
-			err = d.Clear(i)
+			err = d.Clear(iPort)
 			if err == nil {
-				fmt.Printf("unmap port:%d success\n", i)
+				fmt.Printf("unmap port:%d success\n", iPort)
 			}
 		}
 	}
