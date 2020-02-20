3c56aed90c171117db1bbd28e65eab8e99ec971e
vager <chencheng@myshare.io>
2020-02-20 20:54:26 +0800

    add

--- ssh/go.mod
+++ ssh/go.mod
@@ -3,6 +3,6 @@ module myssh
 go 1.13
 
 require (
-	github.com/vager/tools/ssh v0.0.0-20200212082620-12e002952d03 // indirect
+	go.uber.org/zap v1.13.0
 	golang.org/x/crypto v0.0.0-20200214034016-1d94cc7ab1c6
 )
--- ssh/main.go
+++ ssh/main.go
@@ -2,26 +2,90 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
 	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"golang.org/x/crypto/ssh"
+)
+
+var (
+	version   = "unknown"
+	logger    *zap.Logger
+	keyPrefix = "side:failures:"
 )
 
 func main() {
 	user := flag.String("u", "root", "user name")
 	passwd := flag.String("p", "dawter", "user passwd")
 	server := flag.String("s", "192.168.55.2:22", "ssh server")
-	cmd1 := flag.String("cmd1", "ls", "cmd to tun")
+	debug := flag.Bool("debug", false, "debug log level")
+	ver := flag.Bool("version", false, "show version")
+	cmd1 := flag.String("cmd1", "tail -q -n +1 -F --max-unchanged-stats=5 /var/log/vipr/emcvipr-object/dataheadsvc-access.log", "cmd 1 to tun")
 
 	flag.Parse()
+	if *ver {
+		fmt.Println(version)
+		return
+	}
+	logger = initLogger(*debug)
+	defer logger.Sync()
+
+	envInit()
 
 	if err := run(*user, *passwd, *server, *cmd1); err != nil {
 		fmt.Printf("error: %s\n", err)
+		logger.Error("run",
+			zap.String("err", err.Error()),
+		)
 	}
 
+}
+
+func envInit() {
+	// manually set time zone
+	if tz := os.Getenv("TZ"); tz != "" {
+		var err error
+		time.Local, err = time.LoadLocation(tz)
+		if err != nil {
+			logger.Warn("error loading zoneinfo",
+				zap.String("TZ", tz),
+				zap.String("error", err.Error()),
+			)
+		}
+	}
+}
+
+// initLogger init logger
+func initLogger(debug bool) *zap.Logger {
+	zcfg := zap.NewProductionConfig()
+	// Change default(1578990857.105345) timeFormat to 2020-01-14T16:35:34.851+0800
+	zcfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	if debug {
+		zcfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	}
+
+	if os.Getenv("LOGGER") == "file" {
+		filename := filepath.Base(os.Args[0])
+		zcfg.OutputPaths = []string{
+			filepath.Join("/tmp", fmt.Sprintf("%s.log", filename)),
+		}
+	}
+
+	logger, err := zcfg.Build()
+	if err != nil {
+		panic(fmt.Sprintf("initLooger error %s", err))
+	}
+
+	zap.ReplaceGlobals(logger)
+	return logger
 }
 
 func run(user, passwd, server, cmd1 string) error {
@@ -58,25 +122,51 @@ func run(user, passwd, server, cmd1 string) error {
 	if err != nil {
 		return err
 	}
+	go func(r io.Reader) {
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			text := scanner.Text()
+			fields := strings.Split(text, " ")
+			if len(fields) > 12 && fields[1] != "date" {
+				if fields[10] != "-" && (fields[7] == "PUT" || fields[7] == "POST") {
+					logger.Info("got log",
+						zap.String("id", fields[2]),
+						zap.String("ak", fields[5]),
+						zap.String("method", fields[7]),
+						zap.String("bucket", fields[9]),
+						zap.String("object", fields[10]),
+					)
+				}
+			} else {
+				logger.Warn("invalid log",
+					zap.Strings("log", fields),
+				)
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			logger.Error("stdout error",
+				zap.String("err", err.Error()),
+			)
+		}
+	}(outReader)
 
 	errReader, err := session.StderrPipe()
 	if err != nil {
 		return err
 	}
+	go func(r io.Reader) {
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			logger.Warn("got msg from stderr",
+				zap.String("msg", scanner.Text()),
+			)
+		}
+		if err := scanner.Err(); err != nil {
+			logger.Error("stderr error",
+				zap.String("err", err.Error()),
+			)
+		}
+	}(errReader)
 
-	if err := session.Run(cmd1); err != nil {
-		b := &bytes.Buffer{}
-		io.Copy(b, errReader)
-		fmt.Printf("stderr: %s\n", b.String())
-		return err
-	}
-
-	i := 0
-	scanner := bufio.NewScanner(outReader)
-	for scanner.Scan() {
-		//fields := strings.Split(scanner.Text(), " ")
-		fmt.Println(i, scanner.Text())
-		i++
-	}
-	return scanner.Err()
+	return session.Run(cmd1)
 }
