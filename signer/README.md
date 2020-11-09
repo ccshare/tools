a8ff7e753c85a7658400f6ff37d6cb286af8ef4e
shvc <chencheng@myshare.io>
2020-11-09 19:17:19 +0800

    refine

--- signer/README.md
+++ signer/README.md
@@ -1,25 +1,53 @@
 ## S3 v4 signer
 
-- presing
+#### presign
+- s3cli
+```
 s3cli -e http://192.168.55.2:9000 --presign cat open/h0 
 http://192.168.55.2:9000/open/h0?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=object_user1%2F20201030%2Fdefault%2Fs3%2Faws4_request&X-Amz-Date=20201030T080624Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=4ae0512da1671e965d19e264794af9813cbf9bb30dcd5703419006b3977bc948
+```
 
-./signer -s http://192.168.55.2:9000/open/h0
-presign url:  http://192.168.55.2:9000/open/h0?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=object_user1%2F20201030%2Fdefault%2Fs3%2Faws4_request&X-Amz-Date=20201030T080624Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=445d32165e2a551cd09c0cde36834c87511ddb2f93cc66884d8446d9d36425bb
-presign header:  map[host:[192.168.55.2:9000]]
+- signer
+```
+./signer --presign -Xget /open/h0
+header:
+  host:192.168.55.2:9000
+url:
+  http://192.168.55.2:9000/open/h1?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=object_user1%2F20201109%2Fdefault%2Fs3%2Faws4_request&X-Amz-Date=20201109T111506Z&X-Amz-Expires=43200&X-Amz-SignedHeaders=host&X-Amz-Signature=cc1be2b122dbe356d7ce8684b0ad41215c33df897c4105bb6aea0742b5ec33b8
 
+```
 
-- sign
+#### signer
+- s3cli
+```
 s3cli -e http://192.168.55.2:9000 --debug cat open/h0 
-2020/10/30 16:08:25 DEBUG: Request Amazon S3/GetObject Details:
 ---[ REQUEST POST-SIGN ]-----------------------------
 GET /open/h0 HTTP/1.1
 Host: 192.168.55.2:9000
-User-Agent: aws-sdk-go/0.23.0 (go1.14.4; darwin; amd64)
-Amz-Sdk-Invocation-Id: C090A090-094B-4F2C-8C57-D2B4763911AC
+User-Agent: aws-sdk-go/0.24.0 (go1.15.2; darwin; amd64)
+Amz-Sdk-Invocation-Id: 98BE55FD-BECC-4ACE-9A8C-7B9D35FB7CAB
 Amz-Sdk-Request: attempt=1; max=3
-Authorization: AWS4-HMAC-SHA256 Credential=object_user1/20201030/default/s3/aws4_request, SignedHeaders=amz-sdk-invocation-id;amz-sdk-request;host;x-amz-content-sha256;x-amz-date, Signature=889fe6993dd2c21aba4568e6b8c7cd89f9cb832e059f7ee9ababbdd5cdb99472
+Authorization: AWS4-HMAC-SHA256 Credential=object_user1/20201109/default/s3/aws4_request, SignedHeaders=amz-sdk-invocation-id;amz-sdk-request;host;x-amz-content-sha256;x-amz-date, Signature=0a5785d53dedde111f7ec32a905d266f94978b722315292f3900aeeff4f59892
 X-Amz-Content-Sha256: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
-X-Amz-Date: 20201030T080825Z
-Accept-Encoding: gzip
+X-Amz-Date: 20201109T111037Z
+```
 
+- signer
+```
+./signer -t 20201109T111037Z \
+        -H'host:192.168.55.2:9000' \
+        -H'Amz-Sdk-Invocation-Id:98BE55FD-BECC-4ACE-9A8C-7B9D35FB7CAB' \
+        -H'X-Amz-Content-Sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' \
+        -H'Amz-Sdk-Request:attempt=1; max=3' \
+        /open/h0
+header:
+  Amz-Sdk-Invocation-Id:98BE55FD-BECC-4ACE-9A8C-7B9D35FB7CAB
+  X-Amz-Content-Sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+  Amz-Sdk-Request:attempt=1; max=3
+  X-Amz-Date:20201109T111037Z
+  Authorization:AWS4-HMAC-SHA256 Credential=object_user1/20201109/default/s3/aws4_request, SignedHeaders=amz-sdk-invocation-id;amz-sdk-request;host;x-amz-content-sha256;x-amz-date, Signature=93f75717210a961bca66fc68dd00b20ab472695eef6a1b9d92b0a722842f0ae5
+  Host:192.168.55.2:9000
+url:
+  http://192.168.55.2:9000/open/h1
+
+```
--- signer/main.go
+++ signer/main.go
@@ -20,57 +20,78 @@ var (
 )
 
 func printHeader(h http.Header) {
+	fmt.Println("header:")
 	for k, v := range h {
-		fmt.Printf("%s:%s\n", k, strings.Join(v, ","))
+		fmt.Printf("  %s:%s\n", k, strings.Join(v, ","))
 	}
 }
 
-func signV4(region, ak, sk, method, endpoint, path, hash string, presign bool, exp int64) error {
+func presignV4(region, ak, sk, method, endpoint, path, hash string, exp time.Duration) error {
 	req, err := http.NewRequest(method, endpoint, nil)
 	if err != nil {
 		return err
 	}
 	req.URL.Path = path
 
-	v4.NewSigner()
+	cred := credentials.NewStaticCredentialsProvider(ak, sk, "")
+	sign := v4.NewSigner()
+
+	query := req.URL.Query()
+	query.Set("X-Amz-Expires", strconv.FormatInt(int64(exp.Seconds()), 10))
+	req.URL.RawQuery = query.Encode()
+
+	u, h, e := sign.PresignHTTP(context.Background(), cred.Value, req, hash, "s3", region, time.Now())
+	if e != nil {
+		return fmt.Errorf("presign error %w", e)
+	}
+	printHeader(h)
+	fmt.Println("url:\n ", u)
+	return nil
+}
+
+func signV4(region, ak, sk, method, endpoint, path, hash string, header []string, t time.Time) error {
+	req, err := http.NewRequest(method, endpoint, nil)
+	if err != nil {
+		return err
+	}
+	req.URL.Path = path
 
 	cred := credentials.NewStaticCredentialsProvider(ak, sk, "")
 	sign := v4.NewSigner()
 
-	if presign {
-		query := req.URL.Query()
-		query.Set("X-Amz-Expires", strconv.FormatInt(exp, 10))
-		req.URL.RawQuery = query.Encode()
-		u, h, e := sign.PresignHTTP(context.Background(), cred.Value, req, hash, "s3", region, time.Now())
-		if e != nil {
-			return fmt.Errorf("presign error %w", e)
+	for _, v := range header {
+		i := strings.Index(v, ":")
+		if i < 1 || i >= len(v)-1 {
+			return fmt.Errorf("invalid header: %s", v)
 		}
-		printHeader(h)
-		fmt.Println(u)
-	} else {
-		e := sign.SignHTTP(context.Background(), cred.Value, req, "", "s3", region, time.Now())
-		if e != nil {
-			return fmt.Errorf("sign error %w", e)
-		}
-		printHeader(req.Header)
-		fmt.Println(req.URL.String())
+		req.Header.Add(v[:i], v[i+1:])
 	}
+
+	e := sign.SignHTTP(context.Background(), cred.Value, req, hash, "s3", region, t)
+	if e != nil {
+		return fmt.Errorf("sign error %w", e)
+	}
+	printHeader(req.Header)
+	fmt.Println("url:\n ", req.URL.String())
 	return nil
 }
 
 func main() {
 	var endpoint, region, ak, sk string
-	var headers []string
+	var header []string
 	var presign bool
+	var presignExp time.Duration
 	var rootCmd = &cobra.Command{
-		Use:   "sign </bucket/key>",
-		Short: "sign client tool",
+		Use:   "signer </bucket/key>",
+		Short: "signer client tool",
 		Long: `S3 sign tool usage:
 * presign a GET request
+  signer --presign /bucket/key
 * sign a GET request
+  signer /bucket/key
 `,
 		Version: version,
-		Args:    cobra.ExactArgs(1),
+		//Args:    cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			method := strings.ToUpper(cmd.Flag("method").Value.String())
 			switch method {
@@ -81,7 +102,19 @@ func main() {
 			}
 			presign := cmd.Flag("presign").Changed
 			hash := cmd.Flag("hash").Value.String()
-			err := signV4(region, ak, sk, method, endpoint, args[0], hash, presign, 84600)
+			t, err := time.Parse("20060102T150405Z", cmd.Flag("time").Value.String())
+			if err != nil {
+				return fmt.Errorf("invalid time %s %w", cmd.Flag("time").Value.String(), err)
+			}
+
+			//path := args[0]
+			path := "/open/h1"
+			if presign {
+				err = presignV4(region, ak, sk, method, endpoint, path, hash, presignExp)
+			} else {
+				err = signV4(region, ak, sk, method, endpoint, path, hash, header, t)
+			}
+
 			if err != nil {
 				return err
 			}
@@ -94,9 +127,11 @@ func main() {
 	rootCmd.PersistentFlags().StringVarP(&ak, "ak", "", "object_user1", "access key")
 	rootCmd.PersistentFlags().StringVarP(&sk, "sk", "", "ChangeMeChangeMeChangeMeChangeMeChangeMe", "secret key")
 	rootCmd.PersistentFlags().BoolVarP(&presign, "presign", "", false, "presign request")
-	rootCmd.PersistentFlags().StringArrayVarP(&headers, "header", "H", nil, "http headers to sign(-H'content-type:application/json' -H'x-amz-a:b')")
+	rootCmd.PersistentFlags().DurationVarP(&presignExp, "expire", "", 12*time.Hour, "presign URL expiration")
+	rootCmd.PersistentFlags().StringArrayVarP(&header, "header", "H", nil, "http headers to sign(-H'host:b.cc' -H'x-amz-date:20191119T191919Z')")
 	rootCmd.PersistentFlags().StringP("method", "X", http.MethodGet, "http request method")
 	rootCmd.PersistentFlags().StringP("hash", "", "UNSIGNED-PAYLOAD", "body checksum")
+	rootCmd.PersistentFlags().StringP("time", "t", time.Now().UTC().Format("20060102T150405Z"), "signing UTC time")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
