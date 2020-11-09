269c5bd810d60acc452823c44d1ac5fdf8e24bf6
shvc <chencheng@myshare.io>
2020-11-09 10:32:14 +0800

    refine

--- signer/go.mod
+++ signer/go.mod
@@ -3,6 +3,7 @@ module sign
 go 1.15
 
 require (
-	github.com/aws/aws-sdk-go v1.35.22
+	github.com/aws/aws-sdk-go-v2 v0.29.0
+	github.com/aws/aws-sdk-go-v2/credentials v0.1.4
 	github.com/spf13/cobra v1.1.1
 )
--- signer/main.go
+++ signer/main.go
@@ -1,96 +1,77 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/request"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+
 	"github.com/spf13/cobra"
 )
 
 var (
 	version string = "unknown"
-	client  *s3.S3
 )
 
-func newsignent(endpoint, region, ak, sk string, pathStyle bool) (*s3.S3, error) {
-	fmt.Println("ak: ", ak)
-	sess, err := session.NewSession(&aws.Config{
-		Credentials:      credentials.NewStaticCredentials(ak, sk, ""),
-		Endpoint:         &endpoint,
-		Region:           &region,
-		S3ForcePathStyle: &pathStyle,
-	})
-	if err != nil {
-		return nil, err
+func printHeader(h http.Header) {
+	for k, v := range h {
+		fmt.Printf("%s:%s\n", k, strings.Join(v, ","))
 	}
-
-	// Create a new instance of the service's client with a Session.
-	// Optional aws.Config values can also be provided as variadic arguments
-	// to the New function. This option allows you to provide service
-	// specific configuration.
-	svc := s3.New(sess)
-
-	return svc, nil
 }
 
-func sign(name, method, path string, presign bool) error {
-	op := &request.Operation{
-		Name:       name,
-		HTTPMethod: method,
-		HTTPPath:   path,
+func signV4(region, ak, sk, method, endpoint, path, hash string, presign bool, exp int64) error {
+	req, err := http.NewRequest(method, endpoint, nil)
+	if err != nil {
+		return err
 	}
+	req.URL.Path = path
 
-	req := client.NewRequest(op, nil, nil)
+	v4.NewSigner()
+
+	cred := credentials.NewStaticCredentialsProvider(ak, sk, "")
+	sign := v4.NewSigner()
 
 	if presign {
-		s, h, e := req.PresignRequest(86400 * time.Second)
+		query := req.URL.Query()
+		query.Set("X-Amz-Expires", strconv.FormatInt(exp, 10))
+		req.URL.RawQuery = query.Encode()
+		u, h, e := sign.PresignHTTP(context.Background(), cred.Value, req, hash, "s3", region, time.Now())
 		if e != nil {
-			return e
+			return fmt.Errorf("presign error %w", e)
 		}
-		fmt.Println(h)
-		fmt.Println(s)
+		printHeader(h)
+		fmt.Println(u)
 	} else {
-		e := req.Sign()
+		e := sign.SignHTTP(context.Background(), cred.Value, req, "", "s3", region, time.Now())
 		if e != nil {
-			return e
+			return fmt.Errorf("sign error %w", e)
 		}
-		fmt.Println(req.HTTPRequest.Header)
-		fmt.Println(req.HTTPRequest.URL.String())
+		printHeader(req.Header)
+		fmt.Println(req.URL.String())
 	}
 	return nil
 }
 
 func main() {
-	var endpoint, region, ak, sk string
-	var debug, verbose, pathStyle, presign bool
-	var err error
+	var endpoint, region, ak, sk, hash string
+	var headers []string
+	var presign bool
 	var rootCmd = &cobra.Command{
-		Use:   "sign",
+		Use:   "sign </bucket/key>",
 		Short: "sign client tool",
-		Long: `S3 command-line tool usage:
-Endpoint EnvVar:
-	S3_ENDPOINT=http://host:port (only read if flag -e is not set)
-
-Credential EnvVar:
-	AWS_ACCESS_KEY_ID=AK      (only read if flag -p is not set or --ak is not set)
-	AWS_ACCESS_KEY=AK         (only read if AWS_ACCESS_KEY_ID is not set)
-	AWS_SECRET_ACCESS_KEY=SK  (only read if flag -p is not set or --sk is not set)
-	AWS_SECRET_KEY=SK         (only read if AWS_SECRET_ACCESS_KEY is not set)`,
+		Long: `S3 sign tool usage:
+* presign a GET request
+* sign a GET request
+`,
 		Version: version,
 		Args:    cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			client, err = newsignent(endpoint, region, ak, sk, pathStyle)
-			if err != nil {
-				return err
-			}
 			method := strings.ToUpper(cmd.Flag("method").Value.String())
 			switch method {
 			case http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPost, http.MethodDelete:
@@ -99,30 +80,23 @@ Credential EnvVar:
 				return fmt.Errorf("invalid http method: %s", method)
 			}
 			presign := cmd.Flag("presign").Changed
-			var s string
-			var err error
-			//contentType := cmd.Flag("content-type").Value.String()
-			err = sign("GetObject", method, args[0], presign)
+
+			err := signV4(region, ak, sk, method, endpoint, args[0], hash, presign, 84600)
 			if err != nil {
 				return err
 			}
-			fmt.Println(s)
 			return nil
 		},
 	}
-	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "", false, "print debug log")
-	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
 
 	rootCmd.PersistentFlags().StringVarP(&endpoint, "endpoint", "e", "http://192.168.55.2:9000", "S3 endpoint")
 	rootCmd.PersistentFlags().StringVarP(&region, "region", "R", "default", "S3 region")
 	rootCmd.PersistentFlags().StringVarP(&ak, "ak", "", "object_user1", "access key")
 	rootCmd.PersistentFlags().StringVarP(&sk, "sk", "", "ChangeMeChangeMeChangeMeChangeMeChangeMe", "secret key")
-	rootCmd.PersistentFlags().BoolVarP(&pathStyle, "path-style", "", true, "use path style(not use virtual-host)")
 	rootCmd.PersistentFlags().BoolVarP(&presign, "presign", "", false, "presign request")
-
-	rootCmd.PersistentFlags().StringSliceP("header", "H", []string{"content-type:text"}, "http headers")
+	rootCmd.PersistentFlags().StringArrayVarP(&headers, "header", "H", []string{"content-type:text"}, "http headers")
 	rootCmd.PersistentFlags().StringP("method", "X", http.MethodGet, "http request method")
-	rootCmd.PersistentFlags().StringP("content-type", "T", "", "http request content-type")
+	rootCmd.PersistentFlags().StringVarP(&hash, "hash", "", "UNSIGNED-PAYLOAD", "body checksum")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
