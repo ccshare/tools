4b63f8b6c5293d12e158c04eb1174e386b69251e
shvc <chencheng@myshare.io>
2021-04-01 15:19:20 +0800

    add http client

--- fserver/client/main.go
+++ fserver/client/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"time"
 )
 
+var httpClient = http.Client{
+	Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          1000,
+		MaxIdleConnsPerHost:   1000,
+		IdleConnTimeout:       60 * time.Second,
+		TLSHandshakeTimeout:   30 * time.Second,
+		ExpectContinueTimeout: 30 * time.Second,
+		ResponseHeaderTimeout: 30 * time.Second,
+		DisableKeepAlives:     true,
+	},
+	Timeout: 30 * time.Second,
+}
+
 func main() {
-	addr := flag.String("addr", "http://127.0.0.1", "server url")
+	addr := flag.String("addr", "http://127.0.0.1/open/hosts", "server")
 	flag.Parse()
 	fmt.Println(*addr)
 
@@ -16,12 +37,13 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	req.Header.Add("x-htest", " ")
 
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		fmt.Println(err)
 	}
+	err = resp.Body.Close()
+	fmt.Println("close: ", err)
 
 	d, _ := ioutil.ReadAll(resp.Body)
 
